1f3eeec862ace5c237cd055da6b942eb8111c5fe
copecute <copecute10x@gmail.com>
2024-06-21 00:01:26 +0700

    Fix branch merge problem

--- index.php
+++ index.php
@@ -26,14 +26,8 @@ require_once ($_SERVER['DOCUMENT_ROOT'] . '/includes/functions.php');
 renderHeader("Tuyển sinh");
 ?>
 <div class="container">
-<<<<<<< Updated upstream
     <?php if (isset($_SESSION['account_id'])): ?>
         <h2>Chào mừng, <?php echo $username ?>!</h2>
-=======
-    <?php if (checkLogin()): ?>
-        <h2>Chào mừng, <?php echo $user['username']; ?>!</h2>
-        <p><a href="logout.php">Đăng xuất</a></p>
->>>>>>> Stashed changes
     <?php else: ?>
         <h2>Chào mừng đến trang web của chúng tôi!</h2>
     <?php endif; ?>
